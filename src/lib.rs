d2f9516be0c2350fdfb9314750dec6285acd9613
blackpandan <faithgstc54@gmail.com>
2026-02-27 21:19:56 +0100

    NEW: added cli arguments and commands handling with clap

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,45 @@
-enum TicketStatus {
-    Unused,
-    Used,
-    Cancelled,
-}
-
-// struct Event {
-// 	// TODO: use uuid for id
-// 	// TODO: use date type or crate for date
-// 	// TODO: use time type/crate for time
-// 	id: String,
-// 	name: String,
-// 	date: String,
-// 	time: String,
-// 	location: String,
-// }
-
-struct Ticket {
-    // TODO: use uuid for id
-    // TODO: use uuid for event or Event Struct called by uuid
-    // TODO: use encrypted data for signature
-    id: String,
-    event: String,
-    price: f32,
-    status: TicketStatus,
-    signature: String,
-}
-
-impl Ticket {
-    fn new(&self, event: String, price: f32) -> Self {
-        Self {
-            id: String::from("theidtee"),
-            event,
-            price,
-            status: TicketStatus::Unused,
-            signature: "yuagdhopoijfv".to_string(),
-        }
+pub mod ticket_lib {
+    enum TicketStatus {
+        Unused,
+        Used,
+        Cancelled,
     }
 
-    fn verify(ticket_id: &str) {
-        ticket_id.to_string();
+    // struct Event {
+    // 	// TODO: use uuid for id
+    // 	// TODO: use date type or crate for date
+    // 	// TODO: use time type/crate for time
+    // 	id: String,
+    // 	name: String,
+    // 	date: String,
+    // 	time: String,
+    // 	location: String,
+    // }
+
+    pub struct Ticket {
+        // TODO: use uuid for id
+        // TODO: use uuid for event or Event Struct called by uuid
+        // TODO: use encrypted data for signature
+        pub id: String,
+        pub event: String,
+        pub price: f32,
+        status: TicketStatus,
+        signature: String,
+    }
+
+    impl Ticket {
+        pub fn new(event: String, price: f32) -> Self {
+            Self {
+                id: String::from("theidtee"),
+                event,
+                price,
+                status: TicketStatus::Unused,
+                signature: "yuagdhopoijfv".to_string(),
+            }
+        }
+
+        pub fn verify(ticket_id: &str) {
+            ticket_id.to_string();
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use clap::{Parser, Subcommand};
+use ticket_validator::ticket_lib::Ticket;
+
+#[derive(Parser)]
+#[command(
+    name = "Ticket Validation Cli",
+    version = "0.0.1",
+    about = "A tool to create and validate event tickets",
+    long_about = None
+)]
+struct TicketValidationCli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Creates a new ticket
+    Create { name: String, price: f32 },
+
+    /// Scans a ticket and burns it up if unsed
+    Scan { ticket_uuid: String },
+}
+
 fn main() {
+    let cli = TicketValidationCli::parse();
+
     println!("TICKET VALIDATOR");
+
+    match &cli.command {
+        Commands::Create { name, price } => {
+            println!("'create was used', name: {name}, price: {price}");
+            let new_ticket: Ticket = Ticket::new("ut2345hhh".to_string(), 32.00);
+        }
+        Commands::Scan { ticket_uuid } => {
+            println!("'scan was used', ticket_uuid: {ticket_uuid}");
+        }
+    }
 }
