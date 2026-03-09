e27786c8001849705f4109efd104d89b2ee6fcbd
blackpandan <faithgstc54@gmail.com>
2026-03-09 22:24:51 +0100

    NEW: Added event struct to contain information about events

---
+++ src/event.rs
@@ -0,0 +1,9 @@
+// TODO: DateTime
+
+pub struct Event {
+    pub name: String,
+    pub venue: String,
+}
+
+#[cfg(test)]
+mod tests_event {}
--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,6 @@ pub mod cli;
 pub mod crypto;
 pub mod db;
 pub mod errors;
+pub mod event;
 pub mod price;
 pub mod ticket;
