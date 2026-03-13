e9d337f4a8907d6f3835deca9245a6941d652ce0
blackpandan <faithgstc54@gmail.com>
2026-03-13 05:50:28 +0100

    EDIT: added tests and Display impl to event.rs
    - Added Display implementation for Event Struct
    - Added derive for Debug, Clone, and Serialization
    - Added test to ensure proper display

--- src/event.rs
+++ src/event.rs
@@ -1,9 +1,38 @@
-// TODO: DateTime
+use std::fmt;
 
+// TODO: DateTime
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Event {
     pub name: String,
     pub venue: String,
 }
 
+impl fmt::Display for Event {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{},{}", self.name, self.venue)
+    }
+}
+
 #[cfg(test)]
-mod tests_event {}
+mod tests_event {
+    use super::*;
+    use rstest::{fixture, rstest};
+
+    #[fixture]
+    fn event() -> Event {
+        Event {
+            name: "aRT and tEST".into(),
+            venue: "earth-moon, milkyway".into(),
+        }
+    }
+
+    #[rstest]
+    fn test_display(event: Event) {
+        assert_eq!(
+            format!("{},{}", event.name, event.venue),
+            format!("{}", event)
+        );
+    }
+}
